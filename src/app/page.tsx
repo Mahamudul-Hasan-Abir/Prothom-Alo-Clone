43bfb1ee4188909aa9cc46e481af82710ca984fa
Mahamudul Hasan Abir <mhabir10@gmail.com>
2025-07-26 16:37:35 +0600

    adding world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 import Container from "@/components/Container/Container";
 import AdSection from "@/Sections/AdSection";
 import ApnarJonnoSection from "@/Sections/ApnarJonnoSection";
+import Bangladesh from "@/Sections/Bangladesh";
 import MainSection from "@/Sections/MainSection";
+import World from "@/Sections/World";
 
 export default function Home() {
   return (
@@ -14,6 +16,12 @@ export default function Home() {
       <AdSection></AdSection>
       <Container>
         <ApnarJonnoSection></ApnarJonnoSection>
+      </Container>
+      <Container>
+        <Bangladesh></Bangladesh>
+      </Container>
+      <Container>
+        <World></World>
       </Container>
     </div>
   );
